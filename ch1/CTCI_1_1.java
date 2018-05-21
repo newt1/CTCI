bc2af90182dedaa0f017bfc002112ec699752e59
imdatboi <fuentesjorge42@gmail.com>
2018-05-21 00:33:43 -0400

    updated first solution

--- ch1/CTCI_1_1.java
+++ ch1/CTCI_1_1.java
@@ -1,29 +1,28 @@
-import java.util.*; 
 
 public class MyClass {
     //implement an algorithm to determine if a string has all unique characters, no data structures 
     
     public static void main(String args[]) {
     
-        String str1 = "jorgefuentes";  //not unique
+        String str1 = "aaabbbc";  //not unique
         String str2 = "abcdef";     //unique
         
 
-        System.out.println(isUnique(str1));
+        System.out.println(isUnique(str2));
     }
     
     public static boolean isUnique(String str) { 
         if(str == null || str.length() < 2) return true; 
         
         char[] charArray = str.toCharArray(); //n time 
-        Arrays.sort(charArray); 
+        Arrays.sort(charArray); //n log n time
         
         System.out.println( Arrays.toString(charArray) );
         
-        for(int i=0; i<charArray.length-1; i++) { 
+        for(int i=0; i<charArray.length-1; i++) {  //n time 
             if(charArray[i] == charArray[i+1]) return false;    
         }
         
-        return true;         
+        return true;                //total O(n + nlogn)
     }
 }
